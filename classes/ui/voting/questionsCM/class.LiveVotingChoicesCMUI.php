047f34b7e286dcc4942a5d7a7a49dcb13449d89f
Saúl Díaz <sauldiazdiaz210@gmail.com>
2025-05-27 14:06:16 +0200

    Fix: Removed unnecessary htmlspecialchars

--- classes/ui/voting/questionsCM/class.LiveVotingChoicesCMUI.php
+++ classes/ui/voting/questionsCM/class.LiveVotingChoicesCMUI.php
@@ -137,13 +137,13 @@ class LiveVotingChoicesCMUI
             }
 
             $form_answers["hidden"] = $this->factory->input()->field()->hidden()
-                                                    ->withValue(isset($options) ? htmlspecialchars(str_replace('"', "\'", json_encode(array_map(function ($option) {
+                                                    ->withValue(isset($options) ? str_replace('"', "\'", json_encode(array_map(function ($option) {
                                                         return [
                                                             "text" => $option->getText(),
                                                             "id" => $option->getId(),
                                                             "isCorrect" => $option->isCorrect()
                                                         ];
-                                                    }, $options), JSON_UNESCAPED_UNICODE))) : "")
+                                                    }, $options), JSON_UNESCAPED_UNICODE)) : "")
                                                     ->withOnLoadCode(function ($id) {
                                                         return "xlvoForms.initHiddenInput('" . $id . "')";
                                                     })
