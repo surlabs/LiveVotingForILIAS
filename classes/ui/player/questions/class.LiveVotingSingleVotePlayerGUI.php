dccad0110858625d9652ef826fa80c4dc79b85d5
Saúl Díaz <sauldiazdiaz210@gmail.com>
2025-06-09 10:57:22 +0200

    fix: Negative scoring

--- classes/ui/player/questions/class.LiveVotingSingleVotePlayerGUI.php
+++ classes/ui/player/questions/class.LiveVotingSingleVotePlayerGUI.php
@@ -184,11 +184,17 @@ class LiveVotingSingleVotePlayerGUI extends LiveVotingQuestionTypesUI
 
         $time_to_answer = $this->player->getCountdown();
 
-        if ($time_to_answer == 0) {
+        if ($time_to_answer <= 0) {
             $time_to_answer = 1;
         }
 
         $time_remaining = $this->player->remainingCountDown();
+
+        if ($time_remaining < 0) {
+            $time_remaining = 0;
+        } elseif ($time_remaining > $time_to_answer) {
+            $time_remaining = $time_to_answer;
+        }
 
         $score_mult = 1 - (($time_to_answer - $time_remaining)/$time_to_answer)/2;
 
