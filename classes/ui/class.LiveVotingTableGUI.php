dc536f63ef12d29ad66361eec77df961b0d5325b
Saúl Díaz <sauldiazdiaz210@gmail.com>
2025-12-10 14:01:49 +0100

    Update class.LiveVotingTableGUI.php

--- classes/ui/class.LiveVotingTableGUI.php
+++ classes/ui/class.LiveVotingTableGUI.php
@@ -83,7 +83,7 @@ class LiveVotingTableGUI  implements OrderingBinding
     public function getRows(OrderingRowBuilder $row_builder, array $visible_column_ids): Generator
     {
         foreach ($this->records as $record) {
-            $record['question'] = strip_tags($this->shorten($record['question']));
+            $record['question'] = htmlentities($this->shorten($record['question']));
             $record['type'] = $this->plugin->txt('voting_type_' . $record['voting_type']);
 
             yield $row_builder->buildOrderingRow((string) $record['id'], $record);
